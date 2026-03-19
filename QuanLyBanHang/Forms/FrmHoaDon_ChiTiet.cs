bc7605943cd59363ec8e817074f80ccef8b7709d
DTH235620-DinhThanhDanh <danh_dth235620@student.agu.edu.vn>
2026-03-19 19:31:49 +0700

    Add files via upload

---
+++ QuanLyBanHang/Forms/FrmHoaDon_ChiTiet.cs
@@ -0,0 +1,277 @@
+﻿using QuanLyBanHang.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace QuanLyBanHang.Forms
+{
+    public partial class FrmHoaDon_ChiTiet : Form
+    {
+        QLBHDbContext context = new QLBHDbContext();    // Khởi tạo biến ngữ cảnh CSDL 
+        int id;                                         // Lấy mã hóa đơn (dùng cho Sửa và Xóa) 
+        BindingList<DanhSachHoaDon_ChiTiet> hoaDonChiTiet = new BindingList<DanhSachHoaDon_ChiTiet>();
+        PrintDocument printDocument = new PrintDocument();
+
+        public FrmHoaDon_ChiTiet(int maHoaDon = 0)
+        {
+            InitializeComponent();
+            id = maHoaDon;
+        }
+
+        private void FrmHoaDon_ChiTiet_Load(object sender, EventArgs e)
+        {
+            LayNhanVienVaoComboBox();
+            LayKhachHangVaoComboBox();
+            LaySanPhamVaoComboBox();
+
+            dataGridView1.AutoGenerateColumns = false;
+            if (id != 0) // Đã tồn tại chi tiết 
+            {
+                var hoaDon = context.HoaDon.Where(r => r.ID == id).SingleOrDefault();
+                cboNhanVien.SelectedValue = hoaDon.NhanVienID;
+                cboKhachHang.SelectedValue = hoaDon.KhachHangID;
+                txtGhiChuHoaDon.Text = hoaDon.GhiChuHoaDon;
+
+                var ct = context.HoaDon_ChiTiet.Where(r => r.HoaDonID == id).Select(r => new DanhSachHoaDon_ChiTiet
+                {
+                    ID = r.ID,
+                    HoaDonID = r.HoaDonID,
+                    SanPhamID = r.SanPhamID,
+                    TenSanPham = r.SanPham.TenSanPham,
+                    SoLuongBan = r.SoLuongBan,
+                    DonGiaBan = r.DonGiaBan,
+                    ThanhTien = Convert.ToInt32(r.SoLuongBan * r.DonGiaBan)
+                }).ToList();
+
+                hoaDonChiTiet = new BindingList<DanhSachHoaDon_ChiTiet>(ct);
+            }
+
+            dataGridView1.DataSource = hoaDonChiTiet;
+            BatTatChucNang();
+        }
+
+        public void LayNhanVienVaoComboBox()
+        {
+            cboNhanVien.DataSource = context.NhanVien.ToList();
+            cboNhanVien.ValueMember = "ID";
+            cboNhanVien.DisplayMember = "HoVaTen";
+        }
+        public void LayKhachHangVaoComboBox()
+        {
+            cboKhachHang.DataSource = context.KhachHang.ToList();
+            cboKhachHang.ValueMember = "ID";
+            cboKhachHang.DisplayMember = "HoVaTen";
+        }
+        public void LaySanPhamVaoComboBox()
+        {
+            cboSanPham.DataSource = context.SanPham.ToList();
+            cboSanPham.ValueMember = "ID";
+            cboSanPham.DisplayMember = "TenSanPham";
+        }
+
+        public void BatTatChucNang()
+        {
+            if (id == 0 && dataGridView1.Rows.Count == 0) // Thêm 
+            {
+                // Xóa trắng các trường 
+                cboKhachHang.Text = "";
+                cboNhanVien.Text = "";
+                cboSanPham.Text = "";
+                numSoLuongBan.Value = 1;
+                numDonGiaBan.Value = 0;
+            }
+
+            // Nút lưu và xóa chỉ sáng khi có sản phẩm 
+            btnLuuHoaDon.Enabled = dataGridView1.Rows.Count > 0;
+            btnXoa.Enabled = dataGridView1.Rows.Count > 0;
+        }
+
+        private void btnXacNhanBan_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(cboSanPham.Text))
+                MessageBox.Show("Vui lòng chọn sản phẩm.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (numSoLuongBan.Value <= 0)
+                MessageBox.Show("Số lượng bán phải lớn hơn 0.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (numDonGiaBan.Value <= 0)
+                MessageBox.Show("Đơn giá bán sản phẩm phải lớn hơn 0.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            {
+                int maSanPham = Convert.ToInt32(cboSanPham.SelectedValue.ToString());
+                var chiTiet = hoaDonChiTiet.FirstOrDefault(x => x.SanPhamID == maSanPham);
+
+                // Nếu đã tồn tại sản phẩm thì cập nhật thông tin  
+                if (chiTiet != null)
+                {
+                    chiTiet.SoLuongBan = Convert.ToInt16(numSoLuongBan.Value);
+                    chiTiet.DonGiaBan = Convert.ToInt32(numDonGiaBan.Value);
+                    chiTiet.ThanhTien = Convert.ToInt32(numSoLuongBan.Value * numDonGiaBan.Value);
+                    dataGridView1.Refresh();
+                }
+                else // Nếu chưa có sản phẩm thì thêm vào 
+                {
+                    // Nếu chưa có sản phẩm nào 
+                    DanhSachHoaDon_ChiTiet ct = new DanhSachHoaDon_ChiTiet
+                    {
+                        ID = 0,
+                        HoaDonID = id,
+                        SanPhamID = maSanPham,
+                        TenSanPham = cboSanPham.Text,
+                        SoLuongBan = Convert.ToInt16(numSoLuongBan.Value),
+                        DonGiaBan = Convert.ToInt32(numDonGiaBan.Value),
+                        ThanhTien = Convert.ToInt32(numSoLuongBan.Value * numDonGiaBan.Value)
+                    };
+                    hoaDonChiTiet.Add(ct);
+                }
+                BatTatChucNang();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            int maSanPham = Convert.ToInt32(dataGridView1.CurrentRow.Cells["SanPhamID"].Value.ToString());
+            var chiTiet = hoaDonChiTiet.FirstOrDefault(x => x.SanPhamID == maSanPham);
+            if (chiTiet != null)
+            {
+                hoaDonChiTiet.Remove(chiTiet);
+            }
+            BatTatChucNang();
+        }
+
+        private void btnLuuHoaDon_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(cboNhanVien.Text))
+                MessageBox.Show("Vui lòng chọn nhân viên lập hóa đơn.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (string.IsNullOrWhiteSpace(cboKhachHang.Text))
+                MessageBox.Show("Vui lòng chọn khách hàng.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (id != 0) // Đã tồn tại chi tiết thì chỉ cập nhật 
+                {
+                    HoaDon hd = context.HoaDon.Find(id);
+                    if (hd != null)
+                    {
+                        hd.NhanVienID = Convert.ToInt32(cboNhanVien.SelectedValue.ToString());
+                        hd.KhachHangID = Convert.ToInt32(cboKhachHang.SelectedValue.ToString());
+                        hd.GhiChuHoaDon = txtGhiChuHoaDon.Text;
+                        context.HoaDon.Update(hd);
+
+                        // Xóa chi tiết cũ 
+                        var old = context.HoaDon_ChiTiet.Where(r => r.HoaDonID == id).ToList();
+                        context.HoaDon_ChiTiet.RemoveRange(old);
+
+                        // Thêm lại chi tiết mới 
+                        foreach (var item in hoaDonChiTiet.ToList())
+                        {
+                            HoaDon_ChiTiet ct = new HoaDon_ChiTiet();
+                            ct.HoaDonID = id;
+                            ct.SanPhamID = item.SanPhamID;
+                            ct.SoLuongBan = Convert.ToInt16(item.SoLuongBan);
+                            ct.DonGiaBan = item.DonGiaBan;
+                            context.HoaDon_ChiTiet.Add(ct);
+                        }
+
+                        context.SaveChanges();
+                    }
+                }
+                else // Thêm mới 
+                {
+                    HoaDon hd = new HoaDon();
+                    hd.NhanVienID = Convert.ToInt32(cboNhanVien.SelectedValue.ToString());
+                    hd.KhachHangID = Convert.ToInt32(cboKhachHang.SelectedValue.ToString());
+                    hd.NgayLap = DateTime.Now;
+                    hd.GhiChuHoaDon = txtGhiChuHoaDon.Text;
+                    context.HoaDon.Add(hd);
+                    context.SaveChanges();
+
+                    // Thêm chi tiết 
+                    foreach (var item in hoaDonChiTiet.ToList())
+                    {
+                        HoaDon_ChiTiet ct = new HoaDon_ChiTiet();
+                        ct.HoaDonID = hd.ID;
+                        ct.SanPhamID = item.SanPhamID;
+                        ct.SoLuongBan = Convert.ToInt16(item.SoLuongBan);
+                        ct.DonGiaBan = item.DonGiaBan;
+                        context.HoaDon_ChiTiet.Add(ct);
+                    }
+                    context.SaveChanges();
+                }
+
+                MessageBox.Show("Đã lưu thành công!", "Hoàn tất", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void cboSanPham_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int maSanPham = Convert.ToInt32(cboSanPham.SelectedValue.ToString());
+            var sanPham = context.SanPham.Find(maSanPham);
+            numDonGiaBan.Value = sanPham.DonGia;
+        }
+
+        private void InHoaDon()
+        {
+            printDocument.PrintPage += PrintDocument_PrintPage;
+            PrintPreviewDialog preview = new PrintPreviewDialog();
+            preview.Document = printDocument;
+            preview.ShowDialog();
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            float y = 20;
+            Font font = new Font("Arial", 12);
+            Font fontBold = new Font("Arial", 14, FontStyle.Bold);
+
+            // Tiêu đề
+            e.Graphics.DrawString("HÓA ĐƠN BÁN HÀNG", fontBold, Brushes.Black, 200, y);
+            y += 40;
+
+            // Thông tin chung
+            e.Graphics.DrawString("Nhân viên: " + cboNhanVien.Text, font, Brushes.Black, 20, y);
+            y += 25;
+            e.Graphics.DrawString("Khách hàng: " + cboKhachHang.Text, font, Brushes.Black, 20, y);
+            y += 25;
+            e.Graphics.DrawString("Ngày lập: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm"), font, Brushes.Black, 20, y);
+            y += 40;
+
+            // Header bảng
+            e.Graphics.DrawString("Sản phẩm", fontBold, Brushes.Black, 20, y);
+            e.Graphics.DrawString("SL", fontBold, Brushes.Black, 250, y);
+            e.Graphics.DrawString("Đơn giá", fontBold, Brushes.Black, 300, y);
+            e.Graphics.DrawString("Thành tiền", fontBold, Brushes.Black, 400, y);
+            y += 25;
+
+            int tongTien = 0;
+
+            foreach (var item in hoaDonChiTiet)
+            {
+                e.Graphics.DrawString(item.TenSanPham, font, Brushes.Black, 20, y);
+                e.Graphics.DrawString(item.SoLuongBan.ToString(), font, Brushes.Black, 250, y);
+                e.Graphics.DrawString(item.DonGiaBan.ToString("N0"), font, Brushes.Black, 300, y);
+                e.Graphics.DrawString(item.ThanhTien.ToString("N0"), font, Brushes.Black, 400, y);
+
+                tongTien += item.ThanhTien;
+                y += 25;
+            }
+
+            y += 20;
+            e.Graphics.DrawString("TỔNG TIỀN: " + tongTien.ToString("N0") + " VNĐ",
+                fontBold, Brushes.Black, 250, y);
+        }
+
+        private void btnInHoaDon_Click(object sender, EventArgs e)
+        {
+            if (hoaDonChiTiet.Count == 0)
+            {
+                MessageBox.Show("Không có sản phẩm để in.", "Thông báo");
+                return;
+            }
+
+            InHoaDon();
+        }
+    }
+}
---
+++ QuanLyBanHang/Forms/frmSanPham.cs
@@ -0,0 +1,334 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using QuanLyBanHang.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyBanHang.Utils;
+using ClosedXML.Excel;
+
+namespace QuanLyBanHang.Forms
+{
+    public partial class frmSanPham : Form
+    {
+        QLBHDbContext context = new QLBHDbContext();
+        bool xuLyThem = false;
+        int id;
+        string imagesFolder = Application.StartupPath.Replace("bin\\Debug\\net5.0-windows", "Images");
+
+        public frmSanPham()
+        {
+            InitializeComponent();
+        }
+
+        private void BatTatChucNang(bool giaTri)
+        {
+            btnLuu.Enabled = giaTri;
+            btnHuyBo.Enabled = giaTri;
+            cboHangSanXuat.Enabled = giaTri;
+            cboLoaiSanPham.Enabled = giaTri;
+            txtTenSanPham.Enabled = giaTri;
+            numSoLuong.Enabled = giaTri;
+            numDonGia.Enabled = giaTri;
+            txtMoTa.Enabled = giaTri;
+            picHinhAnh.Enabled = giaTri;
+
+            btnThem.Enabled = !giaTri;
+            btnDoiAnh.Enabled = !giaTri;
+            btnSua.Enabled = !giaTri;
+            btnXoa.Enabled = !giaTri;
+            btnTimKiem.Enabled = !giaTri;
+            btnNhap.Enabled = !giaTri;
+            btnXuat.Enabled = !giaTri;
+        }
+
+        public void LayLoaiSanPhamVaoCombobox()
+        {
+            cboLoaiSanPham.DataSource = context.LoaiSanPham.ToList();
+            cboLoaiSanPham.ValueMember = "ID";
+            cboLoaiSanPham.DisplayMember = "TenLoai";
+        }
+
+        public void LayHangSanXuatVaoCombobox()
+        {
+            cboHangSanXuat.DataSource = context.HangSanXuat.ToList();
+            cboHangSanXuat.ValueMember = "ID";
+            cboLoaiSanPham.DisplayMember = "TenHangSanXuat";
+        }
+
+        private void frmSanPham_Load(object sender, EventArgs e)
+        {
+            BatTatChucNang(false);
+            LayLoaiSanPhamVaoCombobox();
+            LayHangSanXuatVaoCombobox();
+
+            dataGridView1.AutoGenerateColumns = false;
+
+            List<DanhSachSanPham> sp = new List<DanhSachSanPham>();
+            sp = context.SanPham.Select(r => new DanhSachSanPham
+            {
+                ID = r.ID,
+                LoaiSanPhamID = r.LoaiSanPhamID,
+                TenLoai = r.LoaiSanPham.TenLoai,
+                HangSanXuatID = r.HangSanXuatID,
+                TenHangSanXuat = r.HangSanXuat.TenHangSanXuat,
+                TenSanPham = r.TenSanPham,
+                SoLuong = r.SoLuong,
+                DonGia = r.DonGia,
+                HinhAnh = r.HinhAnh,
+
+            }).ToList();
+
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = sp;
+
+            cboLoaiSanPham.DataBindings.Clear();
+            cboLoaiSanPham.DataBindings.Add("SelectedValue", bindingSource, "LoaiSanPhamID", false, DataSourceUpdateMode.Never);
+
+            cboHangSanXuat.DataBindings.Clear();
+            cboHangSanXuat.DataBindings.Add("SelectedValue", bindingSource, "HangSanXuatID", false, DataSourceUpdateMode.Never);
+
+            txtTenSanPham.DataBindings.Clear();
+            txtTenSanPham.DataBindings.Add("Text", bindingSource, "TenSanPham", false, DataSourceUpdateMode.Never);
+
+            txtMoTa.DataBindings.Clear();
+            txtMoTa.DataBindings.Add("Text", bindingSource, "MoTa", false, DataSourceUpdateMode.Never);
+
+            numSoLuong.DataBindings.Clear();
+            numSoLuong.DataBindings.Add("Value", bindingSource, "SoLuong", false, DataSourceUpdateMode.Never);
+
+            numDonGia.DataBindings.Clear();
+            numDonGia.DataBindings.Add("Value", bindingSource, "DonGia", false, DataSourceUpdateMode.Never);
+
+            picHinhAnh.DataBindings.Clear();
+            Binding hinhAnh = new Binding("ImageLocation", bindingSource, "HinhAnh");
+            hinhAnh.Format += (s, e) =>
+            {
+                e.Value = Path.Combine(imagesFolder, e.Value.ToString());
+            };
+            picHinhAnh.DataBindings.Add(hinhAnh);
+
+            dataGridView1.DataSource = bindingSource;
+        }
+
+        private void dataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "HinhAnh")
+            {
+                Image image = Image.FromFile(Path.Combine(imagesFolder, e.Value.ToString()));
+                image = new Bitmap(image, 24, 24);
+
+                e.Value = image;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            xuLyThem = true;
+            BatTatChucNang(true);
+            cboLoaiSanPham.Text = "";
+            cboHangSanXuat.Text = "";
+            txtTenSanPham.Clear();
+            txtMoTa.Clear();
+            numSoLuong.Value = 0;
+            numDonGia.Value = 0;
+            picHinhAnh.Image = null;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            xuLyThem = false;
+            BatTatChucNang(true);
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["ID"].Value.ToString());
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận xóa sản phẩm " + txtTenSanPham.Text + "?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==
+            DialogResult.Yes)
+            {
+                id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["ID"].Value.ToString());
+                SanPham sp = context.SanPham.Find(id);
+                if (sp != null)
+                {
+                    context.SanPham.Remove(sp);
+                }
+                context.SaveChanges();
+
+                frmSanPham_Load(sender, e);
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(cboLoaiSanPham.Text))
+                MessageBox.Show("Vui lòng chọn loại sản phẩm.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (string.IsNullOrWhiteSpace(cboHangSanXuat.Text))
+                MessageBox.Show("Vui lòng chọn hãng sản xuất.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (string.IsNullOrWhiteSpace(txtTenSanPham.Text))
+                MessageBox.Show("Vui lòng nhập tên sản phẩm.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (numSoLuong.Value <= 0)
+                MessageBox.Show("Số lượng phải lớn hơn 0.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (numDonGia.Value <= 0)
+                MessageBox.Show("Đơn giá sản phẩm phải lớn hơn 0.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (xuLyThem)
+                {
+                    SanPham sp = new SanPham();
+                    // Tương tự với các form đã thực hiện 
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    SanPham sp = context.SanPham.Find(id);
+                    if (sp != null)
+                    {
+                        // Tương tự với các form đã thực hiện 
+
+                        context.SaveChanges();
+                    }
+                }
+                frmSanPham_Load(sender, e);
+            }
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            frmSanPham_Load(sender, e);
+        }
+
+        private void btnDoiAnh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Cập nhật hình ảnh sản phẩm";
+            openFileDialog.Filter = "Tập tin hình ảnh|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                string ext = Path.GetExtension(openFileDialog.FileName);
+                string fileSavePath = Path.Combine(imagesFolder, fileName.GenerateSlug() + ext);
+                File.Copy(openFileDialog.FileName, fileSavePath, true);
+
+                id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["ID"].Value.ToString());
+                SanPham sp = context.SanPham.Find(id);
+                sp.HinhAnh = fileName.GenerateSlug() + ext;
+                context.SanPham.Update(sp);
+
+                context.SaveChanges();
+                frmSanPham_Load(sender, e);
+            }
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Nhap  du lieu tu tap tin Excel";
+            openFileDialog.Filter = "Tap tin Excel|*.xls;*.xlsx";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    DataTable table = new DataTable();
+                    using (XLWorkbook workbook = new XLWorkbook(openFileDialog.FileName))
+                    {
+                        IXLWorksheet worksheet = workbook.Worksheet(1);
+                        bool firstRow = true;
+                        string readRange = "1:1";
+                        foreach (IXLRow row in worksheet.RowsUsed())
+                        {
+                            //Doc tieu de (dong dau tien)
+                            if (firstRow)
+                            {
+                                readRange = string.Format("{0}:{1}", 1, row.LastCellUsed().Address.ColumnNumber);
+                                foreach (IXLCell cell in row.Cells(readRange))
+                                    table.Columns.Add(cell.Value.ToString());
+                                firstRow = false;
+                            }
+                            else //Doc cac dong noi dung (dong tiep theo)
+                            {
+                                table.Rows.Add();
+                                int cellIndex = 0;
+                                foreach (IXLCell cell in row.Cells(readRange))
+                                {
+                                    table.Rows[table.Rows.Count - 1][cellIndex] = cell.Value.ToString();
+                                    cellIndex++;
+                                }
+                            }
+                        }
+                        if (table.Rows.Count > 0)
+                        {
+                            foreach (DataRow r in table.Rows)
+                            {
+                                SanPham sp = new SanPham();
+                                sp.TenSanPham = r["TenSanPham"].ToString();
+                                context.SanPham.Add(sp);
+                            }
+                            context.SaveChanges();
+
+                            MessageBox.Show("Da nhap thanh cong" + table.Rows.Count + " dong.", "Thanh cong", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            frmSanPham_Load(sender, e);
+                        }
+                        if (firstRow)
+                            MessageBox.Show("Tap tin Excel rong", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Loi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void btnXuat_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Xuất dữ liệu ra tập tin Excel";
+            saveFileDialog.Filter = "Tập tin Excel|*.xls;*.xlsx";
+            saveFileDialog.FileName = "LoaiSanPham_" + DateTime.Now.ToShortDateString().Replace("/", "_") + ".xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    DataTable table = new DataTable();
+
+                    table.Columns.AddRange(new DataColumn[2] {
+                    new DataColumn("ID", typeof(int)),
+                    new DataColumn("TenLoai", typeof(string))
+   });
+
+                    var SanPham = context.LoaiSanPham.ToList();
+                    if (SanPham != null)
+                    {
+                        foreach (var p in SanPham)
+                            table.Rows.Add(p.ID, p.TenLoai);
+                    }
+
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        var sheet = wb.Worksheets.Add(table, "SanPham");
+                        sheet.Columns().AdjustToContents();
+                        wb.SaveAs(saveFileDialog.FileName);
+
+                        MessageBox.Show("Đã xuất dữ liệu ra tập tin Excel thành công.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+    }
+}
